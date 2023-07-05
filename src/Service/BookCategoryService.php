9578147f6a221ee0a89c7cf0f729be64131aed96
max131100 <mkirichenko2308@gmail.com>
2023-07-05 15:53:17 +0300

    Project initialized. Added Book and BookCategory entities

---
+++ src/Service/BookCategoryService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\BookCategory;
+use App\Model\BookCategoryListItem;
+use App\Model\BookCategoryListResponse;
+use App\Repository\BookCategoryRepository;
+use Doctrine\Common\Collections\Criteria;
+
+class BookCategoryService
+{
+    public function __construct(private BookCategoryRepository $repository)
+    {
+    }
+
+    public function getCategories(): BookCategoryListResponse
+    {
+        $categories = $this->repository->findBy([], ['title' => Criteria::ASC]);
+        $items = array_map(
+            fn(BookCategory $bookCategory) => new BookCategoryListItem(
+                $bookCategory->getId(), $bookCategory->getTitle(), $bookCategory->getSlug()
+            ),
+            $categories
+        );
+        return new BookCategoryListResponse($items);
+    }
+}
