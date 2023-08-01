c4b48fbae360be4ed46d4296183af361971cdf8c
max131100 <mkirichenko2308@gmail.com>
2023-08-01 17:03:03 +0300

    Added ability to publish a book and to upload cover-image

---
+++ src/ArgumentResolver/RequestFileArgumentResolver.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\ArgumentResolver;
+
+use App\Attribute\RequestFile;
+use App\Exception\ValidationException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class RequestFileArgumentResolver implements ValueResolverInterface
+{
+    public function __construct(private ValidatorInterface $validator)
+    {
+    }
+
+    public function resolve(Request $request, ArgumentMetadata $argument): iterable
+    {
+        $attributes = $argument->getAttributes(RequestFile::class, ArgumentMetadata::IS_INSTANCEOF);
+        if (!$attributes) {
+            return [];
+        }
+
+        /** @var RequestFile $attribute */
+        $attribute = $attributes[0];
+
+        /** @var UploadedFile $uploadedFile */
+        $uploadedFile = $request->files->get($attribute->getField());
+
+        $errors = $this->validator->validate($uploadedFile, $attribute->getConstraints());
+        if (count($errors) > 0) {
+            throw new ValidationException($errors);
+        }
+
+        return [$uploadedFile];
+    }
+}
--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -3,16 +3,22 @@
 namespace App\Controller;
 
 use App\Attribute\RequestBody;
+use App\Attribute\RequestFile;
 use App\Model\Author\CreateBookRequest;
 use App\Service\AuthorService;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Model\Author\BookListResponse;
 use App\Model\ErrorResponse;
 use App\Model\IdResponse;
+use App\Model\Author\PublishBookRequest;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\NotNull;
+use App\Model\Author\UploadCoverResponse;
 
 class AuthorController extends AbstractController
 {
@@ -24,11 +30,71 @@ class AuthorController extends AbstractController
      * @OA\Tag(name="Author API")
      * @OA\Response(
      *     response=200,
+     *     description="Upload author's book cover",
+     *     @Model(type=UploadCoverResponse::class)
+     * )
+     * @OA\Response(
+     *     response=400,
+     *     description="Validation failed",
+     *     @Model(type=ErrorResponse::class)
+     * )
+     */
+    #[Route(path: '/api/v1/author/book/{id}/uploadCover', methods: ['POST'])]
+    public function uploadCover(
+        int $id,
+        #[RequestFile(field: 'cover', constraints: [
+            new NotNull(),
+            new Image(maxSize: '1M', mimeTypes: ['image/jpeg', 'image/png', 'image/jpg'])
+        ])] UploadedFile $file
+    ): Response {
+        return $this->json($this->authorService->uploadCover($id, $file));
+    }
+
+    /**
+     * @OA\Tag(name="Author API")
+     * @OA\Response(
+     *     response=200,
+     *     description="Publish author's book"
+     * )
+     * @OA\Response(
+     *     response=400,
+     *     description="Validation failed",
+     *     @Model(type=ErrorResponse::class)
+     * )
+     * @OA\RequestBody(@Model(type=PublishBookRequest::class))
+     */
+    #[Route(path: '/api/v1/author/book/{id}/publish', methods: ['POST'])]
+    public function publish(int $id, #[RequestBody] PublishBookRequest $request): Response
+    {
+        $this->authorService->publish($id, $request);
+
+        return $this->json(null);
+    }
+
+    /**
+     * @OA\Tag(name="Author API")
+     * @OA\Response(
+     *     response=200,
+     *     description="Unpublish author's book"
+     * )
+     */
+    #[Route(path: '/api/v1/author/book/{id}/unpublish', methods: ['POST'])]
+    public function unpublish(int $id): Response
+    {
+        $this->authorService->unpublish($id);
+
+        return $this->json(null);
+    }
+
+    /**
+     * @OA\Tag(name="Author API")
+     * @OA\Response(
+     *     response=200,
      *     description="Get author's books",
      *     @Model(type=BookListResponse::class)
      * )
      */
-    #[Route(path: 'api/v1/author/books', methods: ['GET'])]
+    #[Route(path: '/api/v1/author/books', methods: ['GET'])]
     public function books(): Response
     {
         return $this->json($this->authorService->getBooks());
@@ -49,7 +115,7 @@ class AuthorController extends AbstractController
      * )
      * @OA\RequestBody(@Model(type=CreateBookRequest::class))
      */
-    #[Route(path: 'api/v1/author/book', methods: ['POST'])]
+    #[Route(path: '/api/v1/author/book', methods: ['POST'])]
     public function createBook(#[RequestBody] CreateBookRequest $request): Response
     {
         return $this->json($this->authorService->createBook($request));
@@ -67,7 +133,7 @@ class AuthorController extends AbstractController
      *     @Model(type=ErrorResponse::class)
      * )
      */
-    #[Route(path: 'api/v1/author/book/{id}', methods: ['DELETE'])]
+    #[Route(path: '/api/v1/author/book/{id}', methods: ['DELETE'])]
     public function deleteBook(int $id): Response
     {
         $this->authorService->deleteBook($id);
